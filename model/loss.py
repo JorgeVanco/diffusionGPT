a1613daa1f3efe17cbd630ec91215a6a0d1b3a32
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-09-29 19:50:19 -0500

    feat: loss

---
+++ model/loss.py
@@ -0,0 +1,6 @@
+import torch
+from torch import Tensor
+import torch.nn.functional as F
+
+def cdcd_loss(logits: Tensor, target: Tensor) -> Tensor:
+    return F.cross_entropy(logits.view(-1, logits.size(-1)), target.view(-1))
